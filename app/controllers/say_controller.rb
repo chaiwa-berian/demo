6f2bc325dd56874034f03a4a5455a4db27c96e76
Chaiwa Berian <chaiwa.berian@gmail.com>
2015-11-21 14:19:06 +0200

    Initial commit

---
+++ app/controllers/say_controller.rb
@@ -0,0 +1,14 @@
+class SayController < ApplicationController
+  def hello
+  	@time = Time.now
+  	@firstname = "Chaiwa"
+  	@lastname = "Berian"
+
+  	@files = Dir.glob('*')
+
+  	@counter = 0
+  end
+
+  def goodbye
+  end
+end
